0a3aeadaefc2319061e9e76127fa44d9cef0d052
Ray2126 <raymondyang@outlook.co.nz>
2019-12-30 21:49:10 +1300

    Changed nav titles and created fake project server

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import fakeProjectsServer from "../resources/fakeProjectsServer.json";
 
 const Navbar = () => {
   return (
     <div>
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="#">
-          Navbar
+          Bug Tracker
         </a>
-        <button
+        {/* <button
           class="navbar-toggler"
           type="button"
           data-toggle="collapse"
@@ -17,20 +18,20 @@ const Navbar = () => {
           aria-label="Toggle navigation"
         >
           <span class="navbar-toggler-icon"></span>
-        </button>
+        </button> */}
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
+            {/* <li class="nav-item active">
               <a class="nav-link" href="#">
                 Home <span class="sr-only">(current)</span>
               </a>
-            </li>
-            <li class="nav-item">
+            </li> */}
+            {/* <li class="nav-item">
               <a class="nav-link" href="#">
                 Link
               </a>
-            </li>
+            </li> */}
             <li class="nav-item dropdown">
               <a
                 class="nav-link dropdown-toggle"
@@ -41,7 +42,7 @@ const Navbar = () => {
                 aria-haspopup="true"
                 aria-expanded="false"
               >
-                Dropdown
+                Projects
               </a>
               <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                 <a class="dropdown-item" href="#">
@@ -56,7 +57,7 @@ const Navbar = () => {
                 </a>
               </div>
             </li>
-            <li class="nav-item">
+            {/* <li class="nav-item">
               <a
                 class="nav-link disabled"
                 href="#"
@@ -65,9 +66,9 @@ const Navbar = () => {
               >
                 Disabled
               </a>
-            </li>
+            </li> */}
           </ul>
-          <form class="form-inline my-2 my-lg-0">
+          {/* <form class="form-inline my-2 my-lg-0">
             <input
               class="form-control mr-sm-2"
               type="search"
@@ -77,7 +78,7 @@ const Navbar = () => {
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
               Search
             </button>
-          </form>
+          </form> */}
         </div>
       </nav>
     </div>
