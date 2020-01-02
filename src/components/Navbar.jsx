dc7337859c54beb9a8778bf79a01973ad0c3e043
Ray2126 <raymondyang@outlook.co.nz>
2020-01-02 21:10:41 +1300

    Added profile to the right and new project button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,35 +4,13 @@ import fakeProjectsServer from "../resources/fakeProjectsServer.json";
 const Navbar = () => {
   return (
     <div>
-      <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-warning">
         <a className="navbar-brand" href="#">
           Bug Tracker
         </a>
-        {/* <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button> */}
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
-            {/* <li className="nav-item active">
-              <a className="nav-link" href="#">
-                Home <span className="sr-only">(current)</span>
-              </a>
-            </li> */}
-            {/* <li className="nav-item">
-              <a className="nav-link" href="#">
-                Link
-              </a>
-            </li> */}
-
             <li className="nav-item dropdown">
               <a
                 className="nav-link dropdown-toggle"
@@ -45,6 +23,7 @@ const Navbar = () => {
               >
                 Projects
               </a>
+
               <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
                 {fakeProjectsServer.map(project => (
                   <li>
@@ -53,47 +32,50 @@ const Navbar = () => {
                     </a>
                   </li>
                 ))}
-                {/* <li>
-                  <a className="dropdown-item" href="#">
-                    Action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Another action
-                  </a>
-                </li>
-                <div className="dropdown-divider"></div>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </li> */}
               </ul>
             </li>
-
-            {/* <li className="nav-item">
-              <a
-                className="nav-link disabled"
-                href="#"
-                tabindex="-1"
-                aria-disabled="true"
-              >
-                Disabled
-              </a>
-            </li> */}
+            <button className="btn btn-success">New Project</button>
           </ul>
-          {/* <form className="form-inline my-2 my-lg-0">
-            <input
-              className="form-control mr-sm-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
-              Search
-            </button>
-          </form> */}
+          <ul className="navbar-nav">
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                <i className="fas fa-portrait fa-3x"></i>
+              </a>
+
+              <ul
+                className="dropdown-menu dropdown-menu-right text-center"
+                aria-labelledby="navbarDropdown"
+              >
+                <li>
+                  <h4>Username</h4>
+                </li>
+                <hr></hr>
+                <li className="my-1">
+                  <a href="#" className="text-dark">
+                    Profile
+                  </a>
+                </li>
+                <li className="my-1">
+                  <a href="#" className="text-dark">
+                    Settings
+                  </a>
+                </li>
+                <li className="my-1">
+                  <a href="#" className="text-danger">
+                    <span className="font-weight-bold">Sign out</span>
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
         </div>
       </nav>
     </div>
