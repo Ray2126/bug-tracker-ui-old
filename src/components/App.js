32d0c3112233c1d1de6a31a48d43c29edc05dc9e
webber-jiang <webbersjiang@gmail.com>
2020-01-04 18:44:43 +1300

    clean up issues, and create secondNav
    clean up issues, and create secondNav

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import StartPage from "./StartPage";
 import NavBar from "./Navbar";
-import Issues from "./Issues";
+import Issues from "./Issues/Issues";
+import SecondNav from "./SecondNav/SecondNav";
+import TestNav from "./test2ndNav";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
   return (
     <div>
       {/* <StartPage /> */}
       <NavBar />
+      <SecondNav />
       <Issues />
     </div>
   );
